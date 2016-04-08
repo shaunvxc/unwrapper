22ce45a1f6ebe1881cb9c4dff2241e478d713154
shaunvxc <shaun@yipitdata.com>
2016-04-08 11:40:19 -0400

    parse JSON by default

--- tests/unwrap_test.py
+++ tests/unwrap_test.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import sure
+import json
 
 from unwrapper import unwrap
 
 
 def test_basic_unwrap():
-    unwrap("json13123({'a':1, 'b': 2, 'c': 3})").should.equal("{'a':1, 'b': 2, 'c': 3}")
+    unwrap('json13123({"a":1, "b": 2, "c": 3})').should.equal(json.loads('{"a":1, "b": 2, "c": 3}'))
 
 
 def test_unwrap_list():
-    unwrap("json13123([{'a':1, 'b': 2, 'c': 3}, {'d':1, 'e': 2, 'f': 3}])").should.equal("[{'a':1, 'b': 2, 'c': 3}, {'d':1, 'e': 2, 'f': 3}]")
+    unwrap('json13123([{"a":1, "b": 2, "c": 3}, {"d":1, "e": 2, "f": 3}])').should.equal(json.loads('[{"a":1, "b": 2, "c": 3}, {"d":1, "e": 2, "f": 3}]'))
--- unwrapper/application.py
+++ unwrapper/application.py
@@ -2,16 +2,14 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
-
+import json
 
 def unwrap(content):
     starting_symbol = get_start_symbol(content)
     ending_symbol = ']' if starting_symbol == '[' else '}'
-
     start = content.find(starting_symbol, 0)
     end = content.rfind(ending_symbol)
-
-    return content[start:end+1]
+    return json.loads(content[start:end+1])
 
 
 def get_start_symbol(content):
