bc0b8e62ab09f87704208558e330457e33ad4c29
shaunvxc <shaun@yipitdata.com>
2016-04-16 14:10:44 -0400

    fix travis build for python 3.2

--- tests/unwrap_test.py
+++ tests/unwrap_test.py
@@ -8,17 +8,17 @@ from unwrapper import unwrap, unwrap_raw,  CannotFindJsonBoundaryError
 
 def test_basic_unwrap():
     # z = unwrap(u'null({"result":ERROR})')
-    unwrap(u'json13123({"a":1, "b": 2, "c": 3})').should.equal(json.loads('{"a":1, "b": 2, "c": 3}'))
+    unwrap('json13123({"a":1, "b": 2, "c": 3})').should.equal(json.loads('{"a":1, "b": 2, "c": 3}'))
 
 
 def test_unwrap_list():
-    unwrap(u'json13123([{"a":1, "b": 2, "c": 3}, {"d":1, "e": 2, "f": 3}])').should.equal(json.loads('[{"a":1, "b": 2, "c": 3}, {"d":1, "e": 2, "f": 3}]'))
+    unwrap('json13123([{"a":1, "b": 2, "c": 3}, {"d":1, "e": 2, "f": 3}])').should.equal(json.loads('[{"a":1, "b": 2, "c": 3}, {"d":1, "e": 2, "f": 3}]'))
     x = unwrap('json13123([{"a":1, "b": 2, "c": 3}, {"d":1, "e": 2, "f": 3}])')
     assert x[0]["a"] == 1
 
 
 def test_unwrap_raw():
-    unwrap_raw(u'json13123([{"a":1, "b": 2, "c": 3}, {"d":1, "e": 2, "f": 3}])').should.equal('[{"a":1, "b": 2, "c": 3}, {"d":1, "e": 2, "f": 3}]')
+    unwrap_raw('json13123([{"a":1, "b": 2, "c": 3}, {"d":1, "e": 2, "f": 3}])').should.equal('[{"a":1, "b": 2, "c": 3}, {"d":1, "e": 2, "f": 3}]')
 
 
 def test_unwrap_invalid_inputs():
